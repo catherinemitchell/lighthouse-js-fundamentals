2dd00539321b58aa8230b4c8b237272c1402a145
Catherine Mitchell <nicholscat@yahoo.com>
2023-02-14 16:29:53 -0400

    modified some of the exercises

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,34 @@
+const temperature = 12;
+
+if (temperature < 0) {
+    console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+    console.log("Short sleeves won't but it!");
+} else {
+    console.log("Short sleeves are fine.");
+}
+
+const isCitizen = true;
+const age = 26
+
+if (isCitizen && age > 18) {
+    console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea...");
+}
+
+const raining = false
+
+if (!raining) {
+    console.log("Leave your umbrealla at home!");
+}
+
+
+
+
+
 const raining = false
 
 if (!raining) {
